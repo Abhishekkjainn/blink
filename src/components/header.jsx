5bd04c93f98a69dfdf1abf25604146fb9027057b
abhishekkjainn <jainabhishek1904@gmail.com>
2025-02-23 18:51:04 +0530

    Adding page1

---
+++ src/components/header.jsx
@@ -0,0 +1,28 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="company">
+        <img src="./mainlogo.png" alt="Main logo" className="companylogo" />
+        <div className="logotag">
+          <div className="logoname">Blink</div>
+        </div>
+      </div>
+      <div className="links">
+        <div className="link">Dashboard</div>
+        <div className="link">Documentation</div>
+        <div className="link">Contact</div>
+        <div className="link">Reviews</div>
+        <div className="link">Developer</div>
+      </div>
+      <div className="action">
+        <div className="addbutton">
+          <img src="./add.png" alt="add icon" className="addicon" /> Add Link
+        </div>
+        <div className="addbutton">
+          <img src="./scanner.png" alt="add icon" className="addicon" />{' '}
+          Generate QR
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/page1.jsx
@@ -0,0 +1,20 @@
+export default function Page1() {
+  return (
+    <div className="page1">
+      <div className="page1tag">
+        <img src="/mainlogo.png" alt="mainlogo2" className="tagimagepg1" />
+        Short is better
+      </div>
+      <div className="page1head">
+        Shorten and Track
+        <br />
+        Links in a <span className="purple"> Blink.</span>
+      </div>
+      <div className="page1desc">
+        Turn long, cluttered URLs into short, shareable links. Track clicks,
+        analyze performance, and simplify sharing—all in a blink!
+      </div>
+      <div className="page1buttons"></div>
+    </div>
+  );
+}
